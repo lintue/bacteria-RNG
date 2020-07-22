0897fe26d5a1d233a39cf2e6aeb2ebd52614076f
Erik Lintunen <45205520+pixel-tree@users.noreply.github.com>
2020-07-22 20:24:27 +0100

    Update app.py

--- server/app.py
+++ server/app.py
@@ -14,19 +14,7 @@ app = Flask(__name__,
             static_url_path="",
             static_folder=os.path.abspath("../static"))
 
-"""
 # Image to RNG TO DO.
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    message = request.get_data()
-    # message = request.form['message']
-    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
-    fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
-    response_text = {"message":  fulfillment_text}
-
-    return jsonify(response_text)
-"""
-
 
 # General.
 @app.route('/', methods=['GET', 'POST'])
